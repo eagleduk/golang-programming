8cdfbd09b501c8cd3059b3702be28cb94933cd37
eagleduk <eagleduk@gmail.com>
2026-07-01 22:40:31 +0900

    section11 > exercises9 = map 요소 추가

---
+++ section11/09. exercises9.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := map[string][]string{
+		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr":           []string{"Being evil", "Ice cream", "Sunsets"},
+	}
+
+	a["FEF"] = []string{"test", "test1", "test2"}
+
+	for k, v := range a {
+		fmt.Println("======= ", k, " ======")
+		for i, vv := range v {
+			fmt.Printf("\t [%v] = %v \n", i, vv)
+		}
+	}
+}
