c63cdb47504415c626c75c1164a919ac0d092369
eagleduk <eagleduk@gmail.com>
2026-08-13 21:45:38 +0900

    section23 > exercises 2 > question

---
+++ section23/02. exercises2-1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	cs := make(chan<- int)
+
+	go func() {
+		cs <- 42
+	}()
+
+	fmt.Println(<-cs)
+
+	fmt.Println("--------")
+	fmt.Printf("cs\t%T\n", cs)
+}
---
+++ section23/02. exercises2-2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	cr := make(<-chan int)
+
+	go func() {
+		cr <- 42
+	}()
+
+	fmt.Println(<-cr)
+
+	fmt.Println("--------")
+	fmt.Printf("cr\t%T\n", cr)
+}
