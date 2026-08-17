47e5cb7b9ac04d99220d1e07fbbecfa65ba8d464
eagleduk <eagleduk@gmail.com>
2026-08-17 21:52:03 +0900

    section25 > exercises 4 > question

---
+++ section25/04. exercises4.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := exercises_sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func exercises_sqrt(f float64) (float64, error) {
+	if f < 0 {
+	}
+	return 42, nil
+}
