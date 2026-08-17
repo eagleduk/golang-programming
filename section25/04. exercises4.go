2edce781da8e271e3bd6f44075511c387495ee45
eagleduk <eagleduk@gmail.com>
2026-08-17 21:58:19 +0900

    section25 > exercises 4 > solution

--- section25/04. exercises4.go
+++ section25/04. exercises4.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
@@ -19,11 +20,19 @@ func main() {
 	_, err := exercises_sqrt(-10.23)
 	if err != nil {
 		log.Println(err)
+		return
 	}
+
+	fmt.Println("exit of main func")
 }
 
 func exercises_sqrt(f float64) (float64, error) {
 	if f < 0 {
+		return 0, sqrtError{
+			lat:  "lat",
+			long: "long",
+			err:  errors.New(fmt.Sprintf("Less Than 0. %v", f)),
+		}
 	}
 	return 42, nil
 }
